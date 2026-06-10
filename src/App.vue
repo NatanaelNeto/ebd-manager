250a7a6d40e0b81ed009d4c22f81229628ef295b
Natanael Neto <natanael.silva@mv.com.br>
2026-06-10 07:03:45 -0300

    [main] feat: finaliza lista

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,17 @@ import type { GuestType } from "./types/guestType";
 
 
 const dataService = new DataService();
-const data = ref<GuestType[]>([]);
+const guestData = ref<GuestType[]>([]);
 const loadingData = ref(false);
 
 const loadGuestData = async () => {
+  loadingData.value = true;
   try {
-    data.value = await dataService.getData();
-    loadingData.value = false;
+    const data = await dataService.getData();
+    guestData.value = data.sort((a, b) => b.Total - a.Total);
   } catch (error) {
     alert(error);
+  } finally {
     loadingData.value = false;
   }
 }
@@ -33,22 +35,50 @@ onMounted(() => {
         As Crianças da Bíblia
       </p>
     </div>
-    <div class="main-body">
-      <div class="nesn-card ns-shadow ns-rounded">
+    <div class="main-body" v-if="!loadingData && guestData.length > 0">
+      <div v-if="guestData[0]" class="nesn-card ns-shadow ns-rounded nesn-cardlist">
         <div class="nesn-medal ns-gold-medal">
           <p class="text-title">1º</p>
         </div>
+        <div class="nesn-cardlist-name">
+          <h3>
+            {{ guestData[0].Nome }}
+          </h3>
+          <p>{{ guestData[0].Total }} Pontos</p>
+        </div>
       </div>
-      <div class="nesn-card ns-shadow ns-rounded">
+      <div v-if="guestData[1]" class="nesn-card ns-shadow ns-rounded nesn-cardlist">
         <div class="nesn-medal ns-silver-medal">
           <p class="text-title">2º</p>
         </div>
+        <div class="nesn-cardlist-name">
+          <h3>
+            {{ guestData[1].Nome }}
+          </h3>
+          <p>{{ guestData[1].Total }} Pontos</p>
+        </div>
       </div>
-      <div class="nesn-card ns-shadow ns-rounded">
+      <div v-if="guestData[2]" class="nesn-card ns-shadow ns-rounded nesn-cardlist">
         <div class="nesn-medal ns-bronze-medal">
           <p class="text-title">3º</p>
         </div>
+        <div class="nesn-cardlist-name">
+          <h3>
+            {{ guestData[2].Nome }}
+          </h3>
+          <p>{{ guestData[2].Total }} Pontos</p>
+        </div>
       </div>
+      <div v-if="guestData.length > 3" v-for="(guest, index) in guestData.slice(3)" :key="index"
+        class="nesn-card ns-rounded nesn-cardlist">
+        <div class="nesn-cardlist-name">
+          <p class="text-title">{{ guest.Nome }}</p>
+          <p>{{ guest.Total }} Pontos</p>
+        </div>
+      </div>
+    </div>
+    <div v-else-if="loadingData" class="main-body">
+      <h2>Carregando...</h2>
     </div>
   </div>
 </template>
@@ -59,9 +89,9 @@ onMounted(() => {
   height: 100vh;
 
   display: flex;
-  flex-flow: column wrap;
-  justify-content: center;
+  flex-flow: column;
   align-items: center;
+  padding-top: 25vh;
   gap: 20px;
 
   &-header {
@@ -69,6 +99,16 @@ onMounted(() => {
     flex-flow: column wrap;
     justify-content: center;
     align-items: center;
+  }
+}
+
+.nesn {
+  &-cardlist {
+    position: relative;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    gap: 10px;
   }
 }
 </style>
